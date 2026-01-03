5a2dfab929fae02926abfc306d276929c5ac8acf
santilordi <santilordi569@gmail.com>
2026-01-03 20:39:15 -0300

    implement CustomerMapper

---
+++ target/generated-sources/annotations/guru/springframework/msscbrewery/web/mappers/BeerMapperImpl.java
@@ -0,0 +1,49 @@
+package guru.springframework.msscbrewery.web.mappers;
+
+import guru.springframework.msscbrewery.web.domain.Beer;
+import guru.springframework.msscbrewery.web.domain.Beer.BeerBuilder;
+import guru.springframework.msscbrewery.web.model.BeerDto;
+import guru.springframework.msscbrewery.web.model.BeerDto.BeerDtoBuilder;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2026-01-03T20:38:13-0300",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.29 (Microsoft)"
+)
+@Component
+public class BeerMapperImpl implements BeerMapper {
+
+    @Override
+    public BeerDto beerToBeerDto(Beer beer) {
+        if ( beer == null ) {
+            return null;
+        }
+
+        BeerDtoBuilder beerDto = BeerDto.builder();
+
+        beerDto.id( beer.getId() );
+        beerDto.beerName( beer.getBeerName() );
+        beerDto.beerStyle( beer.getBeerStyle() );
+        beerDto.upc( beer.getUpc() );
+
+        return beerDto.build();
+    }
+
+    @Override
+    public Beer beerDtoToBeer(BeerDto beerDto) {
+        if ( beerDto == null ) {
+            return null;
+        }
+
+        BeerBuilder beer = Beer.builder();
+
+        beer.id( beerDto.getId() );
+        beer.beerName( beerDto.getBeerName() );
+        beer.beerStyle( beerDto.getBeerStyle() );
+        beer.upc( beerDto.getUpc() );
+
+        return beer.build();
+    }
+}
---
+++ target/generated-sources/annotations/guru/springframework/msscbrewery/web/mappers/CustomerMapperImpl.java
@@ -0,0 +1,45 @@
+package guru.springframework.msscbrewery.web.mappers;
+
+import guru.springframework.msscbrewery.web.domain.Customer;
+import guru.springframework.msscbrewery.web.domain.Customer.CustomerBuilder;
+import guru.springframework.msscbrewery.web.model.CustomerDto;
+import guru.springframework.msscbrewery.web.model.CustomerDto.CustomerDtoBuilder;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2026-01-03T20:38:13-0300",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.29 (Microsoft)"
+)
+@Component
+public class CustomerMapperImpl implements CustomerMapper {
+
+    @Override
+    public CustomerDto customerToCustomerDto(Customer customer) {
+        if ( customer == null ) {
+            return null;
+        }
+
+        CustomerDtoBuilder customerDto = CustomerDto.builder();
+
+        customerDto.idCustomer( customer.getIdCustomer() );
+        customerDto.nameCustomer( customer.getNameCustomer() );
+
+        return customerDto.build();
+    }
+
+    @Override
+    public Customer customerDtoToCustomer(CustomerDto customerDto) {
+        if ( customerDto == null ) {
+            return null;
+        }
+
+        CustomerBuilder customer = Customer.builder();
+
+        customer.idCustomer( customerDto.getIdCustomer() );
+        customer.nameCustomer( customerDto.getNameCustomer() );
+
+        return customer.build();
+    }
+}
